ef68fcfb99741930d58278693d2dc5e12d6b3582
xietao <xietao02@outlook.com>
2023-11-07 19:54:31 +0800

    Update file structure

--- config.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------- #
-# Author: xietao                					#
-# Repo: https://github.com/xietao02/Federated-NCF/  #
-# ------------------------------------------------- #
-
-
-# Federated Learning Arguments
-NUM_CLIENTS = 10
-FEDERATION_ROUNDS = 50
